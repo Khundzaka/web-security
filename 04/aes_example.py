e72fe58b974142b8e81f8e14b7d4ec4c15a66688
Giorgi Khundzakishvili <khundzaka@gmail.com>
2022-04-05 21:36:58 +0300

    04-05

--- 04/aes_example.py
+++ 04/aes_example.py
@@ -1,4 +1,5 @@
 import hashlib
+# pip install pycryptodome // for installing pycrypto
 from Crypto.Cipher import AES
 from Crypto import Random
 from base64 import b64decode, b64encode
@@ -7,6 +8,20 @@ class MyAES(object):
     def __init__(self, key) -> None:
         self.block_size = AES.block_size
         self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, plain_text):
+        padded_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(padded_text.encode())
+        return b64encode(iv + encrypted_text).decode('utf-8')
+
+    def decrypt(self, encrypted_text):
+        decoded_text = b64decode(encrypted_text)
+        iv = decoded_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(decoded_text[self.block_size:]).decode('utf-8')
+        return self.__unpad(plain_text)
     
     def __pad(self, plain_text):
         number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
@@ -22,9 +37,13 @@ class MyAES(object):
         return plain_text[:-bytes_to_remove]
 
 inst = MyAES('randomkeythatIwanted')
+wrong = MyAES('randomWrongText')
 
 print(inst.block_size)
 print(inst.key)
+encrypted = inst.encrypt('random text')
+print(encrypted)
+print(wrong.decrypt(encrypted))
 
-print(chr(97))
+print(chr(5))
 print(ord('a'))
